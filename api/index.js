f06fae262dbbb13718bd6e0c90ef160b2ac5a193
Gabriel <gabriel@Gabriels-iMac-Pro.local>
2025-09-18 11:59:16 -0300

    add vercel 6

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
-import jsonServer from 'json-server';
+import jsonServer from "json-server";
 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-export default server;
+export default function handler(req, res) {
+  // Faz o json-server lidar com a request e response da Vercel
+  server(req, res);
+}
